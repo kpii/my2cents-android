d32b65abce3ecb08a1a36c74028b91464100a3f3
Anton Rau <anton.rau@gmail.com>
2010-04-12 11:52:57 +0000

    TEMP: trying SVN versioning

--- src/at/my2c/StreamActivity.java
+++ src/at/my2c/StreamActivity.java
@@ -15,7 +15,6 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
-import android.widget.TextView;
 import android.widget.Toast;
 import at.my2c.data.Comment;
 import at.my2c.data.DataManager;
