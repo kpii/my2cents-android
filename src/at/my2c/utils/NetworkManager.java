b4551e28d893dbfcdb656916ea5e474052394cca
Anton Rau <anton.rau@gmail.com>
2010-04-12 19:13:42 +0000

    UPDATE: user agent for HTTP GET requests

--- src/at/my2c/utils/NetworkManager.java
+++ src/at/my2c/utils/NetworkManager.java
@@ -32,7 +32,7 @@ public final class NetworkManager {
 
 	private static final String TAG = "NetworkManager";
 	private static String authToken;
-	
+	private static final String USER_AGENT = "Android my2cents";
 	public static final String BASE_URL = "http://my2cents.mobi";
 	
 	static {
@@ -105,6 +105,7 @@ public final class NetworkManager {
 	public static String queryREST(String url) {
 		HttpClient httpClient = new DefaultHttpClient();
 		HttpGet httpGet = new HttpGet(url);
+		httpGet.setHeader("User-Agent", USER_AGENT);
 		
 		try {
 			HttpResponse response = httpClient.execute(httpGet);			
@@ -139,7 +140,7 @@ public final class NetworkManager {
 		// Create a new HttpClient and Post Header
 		HttpClient httpClient = new DefaultHttpClient();  
 	    HttpPost httpPost = new HttpPost(url);
-	    httpPost.setHeader("User-Agent", "Android my2cents");
+	    httpPost.setHeader("User-Agent", USER_AGENT);
 	    
 	    if ((authToken != null) && (!authToken.equals(""))) {
 	    	httpPost.setHeader("Cookie", authToken);
