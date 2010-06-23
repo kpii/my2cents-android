05665320efaf2898b8033ad80f38e9dff7ca2eec
cmip <cmip@rubbel-desktop.(none)>
2010-06-23 18:26:30 +0200

    added utils/WebViewPool and UserDetailActivity and WebViewActivity

---
+++ src/mobi/my2cents/UserDetailActivity.java
@@ -0,0 +1,15 @@
+package mobi.my2cents;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class UserDetailActivity extends Activity {
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+	    super.onCreate(savedInstanceState);
+	
+	}
+
+}
